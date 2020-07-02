d7b101632c77945e12e44473cd6e6fd618cc6652
Rui Braz <rui.braz.correia@gmail.com>
2020-07-01 23:19:41 -0300

    Adicionado conexão no banco h2 para teste.

--- src/main/java/br/com/braz/rui/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/braz/rui/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package br.com.braz.rui.cursomc.resources;
 
+import br.com.braz.rui.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listar(){
-        return "Rest está funcionando.";
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Infomatica");
+        Categoria cat2 = new Categoria(2, "Escritorio");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
     }
 }

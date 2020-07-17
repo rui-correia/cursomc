08d4d2f58624addd6eb4c57a76af65b2c8e519dd
Rui Braz <rui.braz.correia@gmail.com>
2020-07-16 23:44:58 -0300

    Adicionados Cidade e Estado ao projeto.

---
+++ src/main/java/br/com/braz/rui/cursomc/repositories/EstadoRepositiry.java
@@ -0,0 +1,7 @@
+package br.com.braz.rui.cursomc.repositories;
+
+import br.com.braz.rui.cursomc.domain.Estado;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EstadoRepositiry extends JpaRepository<Estado, Integer> {
+}
